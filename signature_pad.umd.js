aec1e1bc71f234255ccfad1a6caa869829550bc2
이지현 <jhyi@inswave.com>
2023-05-15 17:00:47 +0900

    no message

--- signature_pad.umd.js
+++ signature_pad.umd.js
@@ -479,9 +479,9 @@
       // ctx.lineTo(lastX+10, lastY);
       // ctx.lineTo(circleX2, circleY2 - 2.5);
       
-      ctx.moveTo(circleX, circleY);
+      ctx.moveTo(circleX+radius, circleY);
       ctx.lineTo(lastX, lastY);
-      ctx.lineTo(circleX2, circleY2);      
+      ctx.lineTo(circleX2+radius, circleY2);      
       ctx.fill();
       
       this._data = [];
