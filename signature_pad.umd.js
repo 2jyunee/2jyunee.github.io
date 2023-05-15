57d5577d8cb6df2c7f059d3af3c90b60b2f7e779
이지현 <jhyi@inswave.com>
2023-05-15 17:52:08 +0900

    no message

--- signature_pad.umd.js
+++ signature_pad.umd.js
@@ -480,12 +480,10 @@
       // ctx.moveTo(circleX, circleY + 2.5);
       // ctx.lineTo(lastX+10, lastY);
       // ctx.lineTo(circleX2, circleY2 - 2.5);
-      let t1 = this._strokeWidthByPressure(lastPoints[lastPoints.length-2].pressure);
-      let t2 = this._strokeWidthByPressure(lastPoints[lastPoints.length-1].pressure);
-      
-      ctx.moveTo(circleX, circleY);
-      ctx.lineTo(lastX, lastY + t2);
-      ctx.lineTo(circleX2, circleY2);      
+      //this._strokeWidthByPressure(lastPoints[lastPoints.length-2].pressure);
+      ctx.moveTo(circleX + this._lastWidth/2, circleY);
+      ctx.lineTo(lastX, lastY + this._lastWidth/2);
+      ctx.lineTo(circleX2 - this._lastWidth/2, circleY2);      
       ctx.lineWidth=this._lastWidth ? this._lastWidth : 4;
       ctx.fill();
       
