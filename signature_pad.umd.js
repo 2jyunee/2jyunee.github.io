01158bcbb964b20ed3fac95a448a2491cd959db6
이지현 <jhyi@inswave.com>
2023-05-17 11:28:33 +0900

    no message

--- signature_pad.umd.js
+++ signature_pad.umd.js
@@ -480,6 +480,8 @@
 
       // 두 점 사이 거리
       let d = Math.sqrt(Math.pow(circleX2-circleX, 2) + Math.pow(circleY2-circleY, 2));
+      console.log("d:" + d);
+      console.log("pressure:" + this.pressure);
       
       if(d > this._lastWidth) {
         let tempGap = Math.abs(d - this._lastWidth).toFixed();
@@ -509,9 +511,11 @@
         ctx.lineTo(lastX, lastY);
         ctx.lineTo(circleX2+gap, circleY2);
       } else {
-        ctx.moveTo(circleX-(ctx.lineWidth*0.7), circleY);
+
+        let gap = d > 6.5 ? ctx.lineWidth*0.7 : ctx.lineWidth*0.5;
+        ctx.moveTo(circleX-gap, circleY);
         ctx.lineTo(lastX, lastY);
-        ctx.lineTo(circleX2+(ctx.lineWidth*0.7), circleY2);
+        ctx.lineTo(circleX2+gap, circleY2);
       }
 
       
